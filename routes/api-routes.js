ae31bd80327087f1f9e7362d49809a99bb4374d1
Rahul Nallappa <you@example.com>
2017-05-13 01:28:34 -0500

    started api routes, created public/assets/js/user.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,44 +12,36 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  app.get("/api/authors", function(req, res) {
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
-    db.Author.findAll({
-      include: [db.Post]
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
+  app.get("/api/users", function(req, res) {
+    db.User.findAll({
+    }).then(function(dbTutor) {
+      res.json(dbTutor);
     });
   });
 
-  app.get("/api/authors/:id", function(req, res) {
-    // Here we add an "include" property to our options in our findOne query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
-    db.Author.findOne({
+  app.get("/api/users/:id", function(req, res) {
+    db.Tutor.findOne({
       where: {
         id: req.params.id
       },
-      include: [db.Post]
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
+    }).then(function(dbTutor) {
+      res.json(dbTutor);
     });
   });
 
-  app.post("/api/authors", function(req, res) {
-    db.Author.create(req.body).then(function(dbAuthor) {
-      res.json(dbAuthor);
+  app.post("/api/users", function(req, res) {
+    db.User.create(req.body).then(function(dbUser) {
+      res.json(dbUser);
     });
   });
 
-  app.delete("/api/authors/:id", function(req, res) {
-    db.Author.destroy({
+  app.delete("/api/users/:id", function(req, res) {
+    db.User.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
+    }).then(function(dbUser) {
+      res.json(dbUser);
     });
   });
 

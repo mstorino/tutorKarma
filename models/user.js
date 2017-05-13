df7166a7d9ea25fb284a52b657f15d594be79b81
Karel <karel.hartlieb@gmail.com>
2017-05-13 12:46:19 -0500

    updated user model

--- models/user.js
+++ models/user.js
@@ -4,12 +4,31 @@ var Sequelize = require('sequelize');
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     // Giving the User model a name of type STRING and a role of type STRING
-    name: {
+    firstName: {
     	type: DataTypes.STRING,
     	allowNull: false,
       	validate: {
         	len: [1]
       	}
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+        validate: {
+          len: [1]
+        }
+    },
+    about: {
+      type: DataTypes.TEXT,
+    },
+    photo: {
+      type: DataTypes.STRING,
+    }
+    email: {
+      type: Sequelize.STRING,
+      validate: {
+        isEmail: true
+      }
     },
     role: {
     	type: DataTypes.STRING,
@@ -17,25 +36,8 @@ module.exports = function(sequelize, DataTypes) {
       	validate: {
         	len: [1]
       	}
-    }
+    },
 
-  },
-    // Here we'll pass a second "classMethods" object into the define method
-  {
-      // We're saying that we want our User to have Available times and Subjects
-      classMethods: {
-        associate: function(models) {
-          // Associating User with Availability and Subject
-          // When a user is deleted, also delete any associated availabilities and subjects
-          User.hasMany(models.Availability, {
-            onDelete: "cascade"
-          });
-          User.hasMany(models.Subject, {
-          	onDelete: "cascade"
-          });
-        }
-      }
-    }
-  );
+  });
   return User;
 };

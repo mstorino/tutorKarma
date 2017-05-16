d3aa376dd62f7e62aabb93f9e32fcaf1ed5a8c83
Maggie <test@apples-MacBook-Pro.local>
2017-05-15 19:21:51 -0500

    update seed file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,13 @@
-SELECT DATABASE tutor_db;
+CREATE DATABASE tutor_db;
 
+USE tutor_db;
+
+INSERT INTO subjects (subjectName, createdAt, updatedAt) VALUES
+("Chemistry", "2017-12-22", "2017-12-22");
+INSERT INTO subjects (subjectName, createdAt, updatedAt) VALUES
+("Calculus", "2017-11-21", "2017-12-22");
+INSERT INTO subjects (subjectName, createdAt, updatedAt) VALUES
+("Philosophy", "2017-10-21", "2017-12-22");
 
 
 INSERT INTO Users (firstName, lastName, about, photo, email, role, createdAt, updatedAt) VALUES
@@ -13,20 +21,27 @@ INSERT INTO Users (firstName, lastName, about, photo, email, role, createdAt, up
 
 
 
-INSERT INTO useravailabilities (available, createdAt, updatedAt, UserId, AvailabilityId) VALUES
-(true, "2017-12-12", "2017-11-22", 1, 1);
-INSERT INTO useravailabilities (available, createdAt, updatedAt, UserId, AvailabilityId) VALUES
-(true, "2017-10-12", "2017-11-22", 1, 2);
-INSERT INTO useravailabilities (available, createdAt, updatedAt, UserId, AvailabilityId) VALUES
-(true, "2017-01-12", "2017-02-22",1, 3);
-
-
 INSERT INTO availabilities (day, timeSlot, createdAt, updatedAt) VALUES
 ("Monday", "10:00 AM - 11:00 AM", "2017-12-13", "2017-12-22");
 INSERT INTO availabilities (day, timeSlot, createdAt, updatedAt) VALUES
 ("Tuesday", "10:00 AM - 11:00 AM", "2017-10-13", "2017-07-22");
 INSERT INTO availabilities (day, timeSlot, createdAt, updatedAt) VALUES
 ("Wednesday", "10:00 AM - 11:00 AM", "2017-09-13", "2017-08-22");
+
+
+INSERT INTO sessions (sessionState, createdAt, updatedAt, tutor_id, student_id, SubjectId, AvailabilityId) VALUES
+("scheduled", "2017-09-22", "2017-11-22", 1, 2, 1, 1);
+INSERT INTO sessions (sessionState, createdAt, updatedAt, tutor_id, student_id, SubjectId, AvailabilityId) VALUES
+("scheduled", "2017-05-03", "2017-03-22", 2, 1, 3, 3);
+INSERT INTO sessions (sessionState, createdAt, updatedAt, tutor_id, student_id, SubjectId, AvailabilityId) VALUES
+("scheduled", "2017-05-03", "2017-03-22", 2, 2, 2, 3);
+
+INSERT INTO useravailabilities (available, createdAt, updatedAt, UserId, AvailabilityId) VALUES
+(true, "2017-12-12", "2017-11-22", 1, 1);
+INSERT INTO useravailabilities (available, createdAt, updatedAt, UserId, AvailabilityId) VALUES
+(true, "2017-10-12", "2017-11-22", 1, 2);
+INSERT INTO useravailabilities (available, createdAt, updatedAt, UserId, AvailabilityId) VALUES
+(true, "2017-01-12", "2017-02-22",1, 3);
 
 
 
@@ -43,22 +58,3 @@ UserId, subjectId) VALUES
 (true, "2017-08-17", "2017-08-19",1 ,1);
 
 
-
-
-INSERT INTO subjects (subjectName, createdAt, updatedAt) VALUES
-("Chemistry", "2017-12-22", "2017-12-22");
-INSERT INTO subjects (subjectName, createdAt, updatedAt) VALUES
-("Calculus", "2017-11-21", "2017-12-22");
-INSERT INTO subjects (subjectName, createdAt, updatedAt) VALUES
-("Philosophy", "2017-10-21", "2017-12-22");
-
-
-INSERT INTO sessions (sessionState, createdAt, updatedAt, tutor_id, student_id, SubjectId, AvailabilityId) VALUES
-("scheduled", "2017-09-22", "2017-11-22", 1, 2, 1, 1);
-
-INSERT INTO sessions (sessionState, createdAt, updatedAt, tutor_id, student_id, SubjectId, AvailabilityId) VALUES
-("scheduled", "2017-05-03", "2017-03-22", 2, 1, 3, 3);
-
-
-INSERT INTO sessions (sessionState, createdAt, updatedAt, tutor_id, student_id, SubjectId, AvailabilityId) VALUES
-("scheduled", "2017-05-03", "2017-03-22", 2, 2, 2, 3);

2ed59f18bc6933e65fda0e5affa389511766e03d
Maggie <test@apples-MBP.attlocal.net>
2017-05-18 00:58:03 -0500

    fix sort by subject and tutor features

--- public/assets/js/student.js
+++ public/assets/js/student.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
   
   $('#sel1').on("change", handleTutorFormSubmit);
  
-  $('.closeBtn').on("click", resetState);
+  // $('.closeBtn').on("click", resetState);
+  $('#sel2').on("change", handleSubjectFormSubmit);
 
   function handleTutorFormSubmit(event) {
     event.preventDefault();
@@ -33,7 +34,7 @@ function parseTutorData (data) {
  
        }    
 
-      console.log(allTutors[i].id);
+      // console.log(allTutors[i].id);
     }
 }
 
@@ -51,12 +52,59 @@ function resetState(){
 
 // ************* Subject Button
 
-$('#sel2').on("change", handleSubjectFormSubmit);
+var selectedValue2;
+var userIdSubjTutor = [];
+var tutorRow = "";
+
+function resetTutorsBySubject(){
+  selectedValue2 = 0;
+  userIdSubjTutor = [];
+  tutorRow = "";
+}
 
 function handleSubjectFormSubmit(event) {
     event.preventDefault();
-    console.log(allSubjects);
+    // console.log(allSubjects);
+    var selectBox2 = document.getElementById("sel2");
+    selectedValue2 = selectBox2.options[selectBox2.selectedIndex].value;
+
+    if (!selectedValue2) {
+      return;
+    }
+    
+    parseTutorBySubject(allTutors)
+    resetTutorsBySubject();
 
 }
+
+
+function parseTutorBySubject (data) {
+  console.log(selectedValue2);
+    for (var i = 0; i < allTutors.length; i++) {
+      // console.log(parseInt(allTutors[i].SubjectId))
+      if (parseInt(selectedValue2) === parseInt(allTutors[i].SubjectId)) {
+        userIdSubjTutor.push(allTutors[i]);
+       }    
+    }
+  createTutorRow(userIdSubjTutor);
+}
+
+function createTutorRow(data) {
+    for (var i = 0; i < data.length; i++) {
+      tutorRow += "<div class='container tutorThumbnailContainer col-xs-6 col-md-3 col-lg-3'><div class='thumbnail tutorThumbnail'><img src ='assets/img/tutor" + data[i].id + ".jpg'><div class='tutorCaption'><h4 class = 'capitalize tutorName'>" + data[i].firstName + "</h4><p>" + data[i].about + "</p><a href='#' class='btn btn-default btn-sm capitalize bookTutorBtn' role='button'>Book" + data[i].firstName + "</a></div></div></div>"
+    }
+    showTutorRow(tutorRow); 
+
+  }
+
+
+function showTutorRow(data) {
+   $('.tutorRow').html(data);
+}
+
+
+
+// ************* Book Tutor Button
+
 
 }); 

2bf57c9a4d0dface93dfd9be3f16f4073e9a3689
Karel <karel.hartlieb@gmail.com>
2017-05-18 11:10:37 -0500

    tutor stuff

---
+++ routes/api-routes/userSubjects-routes.js
@@ -0,0 +1,45 @@
+
+var db = require("../../models");
+// Routes
+// =============================================================
+module.exports = function(app) {
+  app.get("/api/usersubjects", function(req, res) {
+    db.Subject.findAll({
+    }).then(function(dbUserSubject) {
+      res.json(dbUserSubject);
+    });
+  });
+
+  app.get("/api/usersubjects/:SubjectId", function(req, res) {
+    db.Subject.findAll({
+    	where: {
+    		SubjectId: req.params.SubjectId
+    	}
+    }).then(function(dbUserSubject) {
+      res.json(dbUserSubject);
+    });
+  });
+
+// var db = require("../../models");
+// // Routes
+// // =============================================================
+// module.exports = function(app) {
+//   app.get("/api/usersubjects", function(req, res) {
+//     db.UserSubject.findAll({
+//     }).then(function(dbUserSubject) {
+//       res.json(dbUserSubject);
+//     });
+//   });
+
+//   app.get("/api/usersubjects/:SubjectId", function(req, res) {
+//     db.UserSubject.findAll({
+//     	where: {
+//     		SubjectId: req.params.SubjectId
+//     	}
+//     }).then(function(dbUserSubject) {
+//       res.json(dbUserSubject);
+//     });
+//   });
+
+
+// };

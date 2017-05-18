ed0f5844d2752268e236152ef85ee4456ef5a1f0
Maggie <test@apples-MBP.attlocal.net>
2017-05-17 23:46:31 -0500

    create admin page w/ all tutor data

--- routes/app-routes.js
+++ routes/app-routes.js
@@ -27,20 +27,20 @@ module.exports = function(app) {
     });
   });
 
-app.get("/student/id", function (req, res) {
+// app.get("/student/id", function (req, res) {
    
-     db.User.findAll({
-        where: {
-         id: req.params.id 
-        }
-     }).then(function(dbSpecificTutor){
-        res.render("student/id", {
-        availTutor: dbSpecificTutor
-      // console.log(dbTutors);
+//      db.User.findAll({
+//         where: {
+//          id: req.params.id 
+//         }
+//      }).then(function(dbSpecificTutor){
+//         res.render("student/id", {
+//         availTutor: dbSpecificTutor
+//       // console.log(dbTutors);
         
-      }); 
-    });
-  });
+//       }); 
+//     });
+//   });
 
 
   app.get("/tutor", function (req, res) {
@@ -48,7 +48,21 @@ app.get("/student/id", function (req, res) {
   });
 
   app.get("/admin", function (req, res) {
-    res.render("admin")
+   db.Subject.findAll({
+   }).then(function(dbSubjects){
+    
+     db.User.findAll({
+        
+     }).then(function(dbTutors){
+        res.render("admin", {
+        availSubject: dbSubjects,
+        availTutor: dbTutors,
+        availTutorArray: JSON.stringify(dbTutors),
+        availSubjArray: JSON.stringify(dbSubjects)
+      
+        })
+      }); 
+    });
   });
 
 };
